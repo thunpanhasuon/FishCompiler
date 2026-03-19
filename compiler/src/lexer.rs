0345fc11435ff10b9fc3f607cd08ccf061d60855
thunpanhasuon <thunpanhasuon@gmail.com>
2026-03-19 20:19:51 +0700

    parse token

--- compiler/src/lexer.rs
+++ compiler/src/lexer.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io;
 use std::path::Path;
+use std::mem::take;
 use std::io::BufReader;
 use std::io::BufRead;
 
@@ -9,7 +10,52 @@ pub enum Token {
     Number(i64),
     Operator(char),
 }
+pub struct Lexer {
+    tokens: Vec<Token>,
+}
+impl Lexer {
+    pub fn new() -> Self {
+        Lexer {
+            tokens: Vec::new(),
+        }
+    }
+    pub fn tokenize(&mut self, input: Vec<String>) -> Vec<Token> {
+         for s in input {
+            let mut iter = s.chars().peekable();
+            
+            while let Some(&c) = iter.peek() {
+                match c {
+                    _ if c.is_whitespace() => {
+                        iter.next();
+                    }
 
+                    '0'..='9' => {
+                        let mut number = String::new();
+                        while let Some(&nc) = iter.peek() {
+                           if nc.is_alphanumeric() {
+                               number.push(iter.next().unwrap());
+                           } else {
+                            break;
+                           }
+                        }
+                        let num_value: i64 = number.parse().unwrap();
+                        self.tokens.push(Token::Number(num_value));
+                    }
+                    
+                    '+' | '-' | '*'| '/' => {
+                        self.tokens.push(Token::Operator(iter.next().unwrap()));
+                    }
+
+                    _ => {
+                        println!("Error: Unknown character {}", iter.next().unwrap());
+                        break;                  
+                    }
+                }
+            }
+         }
+        take(&mut self.tokens)
+    }
+}
 /* create tokens */
 pub fn read(path: impl AsRef<Path>) -> Result<Vec<String>, io::Error> {
     let file = File::open(path)?;
@@ -18,6 +64,7 @@ pub fn read(path: impl AsRef<Path>) -> Result<Vec<String>, io::Error> {
    
     reader.lines().collect()
 }
+
 pub fn scan(strings: Vec<String>) {
      for s in strings {
          let mut iter = s.chars().peekable();
--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -2,6 +2,7 @@ mod lexer;
 
 use crate::lexer::read;
 use crate::lexer::scan;
+
 use std::env;
 
 fn main() {
@@ -10,10 +11,13 @@ fn main() {
     println!("reading from DIR: {:?}", env::current_dir().unwrap());
 
     let path = "program1.fsh";
-
+    
     if std::path::Path::new(path).exists() {
         let lines = read(path);
-        scan(lines.expect("only char allow"));
+        let mut lexer_parse = lexer::Lexer::new();
+        let tokens = lexer_parse.tokenize(lines.expect("only char allow"));
+        println!("Tokens: {:?}", tokens);
+        // scan(lines.expect("only char allow"));
         
     } else {
         println!("Error: Rust cannot find the file at {:?}", path);
