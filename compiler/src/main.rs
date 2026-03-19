492eb1f98b988ece70a18aaa5c78a247dbd9f2ad
panhaUwU <devlopoer@gmail.com>
2026-03-19 22:39:17 +0700

    trying to add pratt-parser can you also help me with this one

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -1,13 +1,9 @@
 mod lexer;
-
 use crate::lexer::read;
-use crate::lexer::scan;
-
 use std::env;
 
 fn main() {
     
-    /* debug this out */
     println!("reading from DIR: {:?}", env::current_dir().unwrap());
 
     let path = "program1.fsh";
